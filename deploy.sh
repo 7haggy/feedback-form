bbd5bfb93f77e5dd4d386dd1b6ddb3ae1d051a64
Vlad <vlad.shaginyan@mail.ru>
2020-08-03 02:18:12 +0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+# abort on errors
+set -e
+
+# build
+yarn run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:7haggy/feedback-form.git master:gh-pages
+
+cd -
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    publicPath: "/feedback-form/",
+};
